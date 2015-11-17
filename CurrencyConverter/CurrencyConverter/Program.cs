2064fc0414f9d420a8a135ce1ad5c5ff2f72dc0c
RajeevArora93 <rajeevarora@live.co.uk>
2015-11-17 17:09:52 +0000

    XmlReadFile

--- CurrencyConverter/CurrencyConverter/Program.cs
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -10,6 +10,9 @@ namespace CurrencyConverter
     {
         static void Main(string[] args)
         {
+            ReadRates r = new ReadRates();
+            r.ReadXmlFile("C:/Users/rajeev.arora/Source/Repos/CurrencyConversionProject/CurrencyConverter/CurrencyRates.xml");
+            Console.ReadLine();
         }
     }
 }
---
+++ CurrencyConverter/CurrencyConverterTests/ReadRatesTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CurrencyConverter;
+
+namespace CurrencyConverterTests
+{
+    [TestClass]
+    public class ReadRatesTest
+    {
+        [TestMethod]
+        public void Test_ReadXmlFile_XmlFileHasBeenRead_WhenReadXmlFileIsCalled()
+        {
+            ReadRates reader = new ReadRates();
+
+            reader.ReadXmlFile("C:/Users/rajeev.arora/Source/Repos/CurrencyConversionProject/CurrencyConverter/CurrencyRates.xml");
+
+           // Assert.AreEqual( );
+        }
+    }
+}
