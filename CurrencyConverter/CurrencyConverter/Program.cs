62b121f3c56d25c976774dfbb9f42eda3563d3e5
RajeevArora93 <rajeevarora@live.co.uk>
2015-11-18 12:22:36 +0000

    NewXmlReader

--- CurrencyConverter/CurrencyConverter/Program.cs
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -10,8 +10,10 @@ namespace CurrencyConverter
     {
         static void Main(string[] args)
         {
-            ReadRates r = new ReadRates();
-            r.ReadXmlFile("C:/Users/rajeev.arora/Source/Repos/CurrencyConversionProject/CurrencyConverter/CurrencyRates.xml");
+            ReadRates r = new ReadRates("C:/Users/rajeev.arora/Source/Repos/CurrencyConversionProject/CurrencyConverter/CurrencyRate.xml"); 
+
+            r.GetRatesData();
+
             Console.ReadLine();
         }
     }
--- CurrencyConverter/CurrencyConverterTests/ReadRatesTest.cs
+++ CurrencyConverter/CurrencyConverterTests/ReadRatesTest.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CurrencyConverter;
+using System.Collections.Generic;
 
 namespace CurrencyConverterTests
 {
     [TestClass]
     public class ReadRatesTest
     {
+       
         [TestMethod]
-        public void Test_ReadXmlFile_XmlFileHasBeenRead_WhenReadXmlFileIsCalled()
+        public void Text_GetRatesData_ItemsHaveBeenAddedToTheDictionary_WhenXmlDataHasBeenAdded()
         {
-            ReadRates reader = new ReadRates();
+            string path = "C:/Users/rajeev.arora/Source/Repos/CurrencyConversionProject/CurrencyConverter/CurrencyRates.xml";
+            ReadRates reader = new ReadRates(path);
 
-            reader.ReadXmlFile("C:/Users/rajeev.arora/Source/Repos/CurrencyConversionProject/CurrencyConverter/CurrencyRates.xml");
+            Dictionary<Tuple<int, string>, Tuple<string, string>> currencies = reader.GetRatesData();
 
-           // Assert.AreEqual( );
+            Assert.AreNotEqual(0, currencies.Count);
         }
     }
 }
